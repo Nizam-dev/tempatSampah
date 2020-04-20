92d73829e7d09c1071e93e18165d06bad41c755b
Tithoyulidharsatriantama <64019744+Tithoyulidharsatriantama@users.noreply.github.com>
2020-04-20 21:42:30 +0700

    titho yulidhar s
    tambah tempat sampah 2 dan 3

--- sampah.js
+++ sampah.js
@@ -20,6 +20,33 @@ function draw() {
     context.fillStyle = 'yellow';
     context.fillRect(70,225,25,25);
     context.restore();
+	
+	// tempat sampah 2 dan 3 
+	context.save();
+     context.fillStyle = a;
+     context.fillRect(160,250,100,150);
+     context.translate(265,245);
+     context.rotate((Math.PI/180)*35);
+     context.scale(-1,1);
+     context.fillStyle= 'blue';
+     context.fillRect(0,0,100,10);
+     context.fillStyle = 'yellow';
+     context.fillRect(35,-25,25,25);
+     context.restore();
+
+
+
+  context.save();
+     context.fillStyle = a;
+     context.fillRect(290,250,100,150);
+     context.translate(390,250);
+     context.rotate((Math.PI/180)*90);
+     context.scale(-1,1);
+     context.fillStyle= 'blue';
+     context.fillRect(0,0,100,10);
+     context.fillStyle = 'yellow';
+     context.fillRect(35,-25,30,25);
+     context.restore();
 				
 
  }
